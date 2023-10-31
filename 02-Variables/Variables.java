55afa0ea780dab15267599ea9e593cd8794f2ce6
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2023-11-01 00:45:55 +0530

    Create Variables.java

---
+++ 02-Variables/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args){
+        //Variables
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        int mul=a*b;
+        System.out.println(sum);
+        System.out.print(mul);
+    }
+    
+}
