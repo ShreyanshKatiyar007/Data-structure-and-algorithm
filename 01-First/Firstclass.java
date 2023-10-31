e4affdc658fe10861c9124598157a690265d045b
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2023-11-01 00:39:21 +0530

    Create Firstclass.java

---
+++ 01-First/Firstclass.java
@@ -0,0 +1,8 @@
+public class Firstclass {
+    public static void main(String args[]) {
+        System.out.println("hello World") ;
+        System.out.println("Today is the Good day") ;
+          }
+    
+}
+ 
