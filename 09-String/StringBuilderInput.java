575a886422cd69e9cc396c4f9c72d0b8df820ea5
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2023-11-03 01:26:09 +0530

    Create StringBuilderInput.java

---
+++ 09-String/StringBuilderInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class StringBuilderInput{
+    public static void main(String args[]) {
+        // Declaration of StringBuilder.
+        StringBuilder sb = new StringBuilder("Shreyansh");
+        System.out.println(sb);
+    }
+}
