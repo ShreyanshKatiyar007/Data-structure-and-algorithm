6498a34e02f7108bc34991b5e18ef2e02ea0b0a1
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2023-11-03 00:27:46 +0530

    Create StringProperties.java

---
+++ 09-String/StringProperties.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StringProperties {
+    public static void main(String args[]) {
+        // Concatenation - Use to add two strings.
+        String firstname = "Shreyansh";
+        String lastname = "Katiyar";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+        // length - Use to calculate length of the string.
+        System.out.println("Length of the string is:" + fullname.length());
+        // CharAt - Use to write string character saperately.
+        for (int i = 0; i < fullname.length(); i++) {
+            System.out.println(fullname.charAt(i));
+        }
+        // CompareTo - Use to compare two strings.
+        if (firstname.compareTo(lastname) == 0) {
+            System.out.println("String are equal");
+        } else {
+            System.out.println("String are not equal");
+        }
+        // Substring - Use to take small part of a string.
+        String name = firstname.substring(5, firstname.length());
+        System.out.println(name);
+    }
+}
