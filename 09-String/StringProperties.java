931cee6f136c3927aa82ce2bf9a4a8e32c30d23c
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2023-11-03 01:56:42 +0530

    Update StringProperties.java

--- 09-String/StringProperties.java
+++ 09-String/StringProperties.java
@@ -7,19 +7,23 @@ public class StringProperties {
         String lastname = "Katiyar";
         String fullname = firstname + " " + lastname;
         System.out.println(fullname);
-        // length - Use to calculate length of the string.
+        
+        // length() - Use to calculate length of the string.
         System.out.println("Length of the string is:" + fullname.length());
-        // CharAt - Use to write string character saperately.
+        
+        // CharAt() - Use to write string character saperately.
         for (int i = 0; i < fullname.length(); i++) {
             System.out.println(fullname.charAt(i));
         }
-        // CompareTo - Use to compare two strings.
+        
+        // CompareTo() - Use to compare two strings.
         if (firstname.compareTo(lastname) == 0) {
             System.out.println("String are equal");
         } else {
             System.out.println("String are not equal");
         }
-        // Substring - Use to take small part of a string.
+        
+        // Substring() - Use to take small part of a string.
         String name = firstname.substring(5, firstname.length());
         System.out.println(name);
     }
