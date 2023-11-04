0791f59301b30810001fe6a776cb1bdf3f788f5a
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2023-11-04 18:04:22 +0530

    Update and rename StringBuilderInput.java to Input.java

--- 09-String/StringBuilder/Input.java
+++ 09-String/StringBuilder/Input.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StringBuilderInput{
+public class Input{
     public static void main(String args[]) {
         // Declaration of StringBuilder.
         StringBuilder sb = new StringBuilder("Shreyansh");
