1c50ae360b1508f7813dbf5fd74db7b1d5605657
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2023-11-05 00:35:14 +0530

    Create ClearBit.java

---
+++ 10-BitManipulation/ClearBit.java
@@ -0,0 +1,12 @@
+public class ClearBit {
+    public static void main(String[] args) {
+        // 3--> Get Bit Operation
+        int n = 5; // 0101
+        int pos = 2;
+        int bitmask = 1 << pos;
+
+        int notBitmask = ~(bitmask);
+        int number = notBitmask & n;
+        System.out.println(number);
+    }
+}
