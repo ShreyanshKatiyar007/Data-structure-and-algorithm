05f97513d6ca919a615a6dff9df8aaa934053851
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2023-11-05 00:23:05 +0530

    Create SetBit.java

---
+++ 10-BitManipulation/SetBit.java
@@ -0,0 +1,11 @@
+public class SetBit {
+    public static void main(String[] args) {
+        // 2--> Set Bit Operation
+        int n = 5;
+        int pos = 1;
+        int bitmask = 1 << pos;
+
+        int newNumber = bitmask | n;
+        System.out.println(newNumber);
+    }
+}
