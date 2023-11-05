51914df5c215a61eaa21c5de6f63f9d193e982ee
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2023-11-05 19:32:01 +0530

    Create BubbleSort.java

---
+++ 11-Sorting/BubbleSort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Bubblesort {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Enter the size of the array:");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
