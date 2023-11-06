64e5d0a74b0efac033a087ccae6f76185ca4dca9
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2023-11-06 23:58:18 +0530

    Create SelectionSort.java

---
+++ 11-Sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class SelectionSort {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Enter the size of the array:");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }
+}
