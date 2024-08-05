34822a456b126fa81ce6eb21012319a67d965cc9
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-08-05 23:50:19 +0530

    Create Array.cpp

---
+++ C++/Array/Array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[3] = {1, 2, 3};
+    int num[10];
+    int first[100] = {0};
+    for (int i = 0; i <= 2; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    int size = sizeof(arr) / sizeof(int);
+    cout << "\nSize of arr is:" << size << endl;
+    cout << "Everything is fine";
+}
