0a224efdeb55da700477b5ada86df8d56c965875
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-08-16 22:37:05 +0530

    Create fucntions.cpp

---
+++ C++/Pointers/Double Pointers/fucntions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void update(int **p2)
+{
+
+    // p2 = p2 + 1;
+    //*p2 = *p2 + 1;
+    **p2 = **p2 + 1;
+}
+int main()
+{
+
+    int i = 5;
+    int *p = &i;
+    int **p2 = &p;
+
+    cout << "before" << i << endl;
+    cout << "before" << p << endl;
+    cout << "before" << p2 << endl;
+    cout << endl;
+
+    update(p2);
+
+    cout << endl;
+    cout << "after" << i << endl;
+    cout << "after" << p << endl;
+    cout << "after" << p2 << endl;
+}
