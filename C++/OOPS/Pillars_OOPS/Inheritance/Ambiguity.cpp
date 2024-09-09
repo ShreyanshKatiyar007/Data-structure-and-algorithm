7edfd423b764e71ddbbf52ff0f6e9cfed1728d1e
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-09-09 23:28:48 +0530

    Create Ambiguity.cpp

---
+++ C++/OOPS/Pillars_OOPS/Inheritance/Ambiguity.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+
+public:
+    void func()
+    {
+        cout << "Inside Class A" << endl;
+    }
+};
+
+class B
+{
+
+public:
+    void func()
+    {
+        cout << "Inside Class B" << endl;
+    }
+};
+
+class C : public A, public B
+{
+};
+
+int main()
+{
+
+    C obj;
+    obj.A::func(); // resolving ambiguity
+    obj.B::func();
+}
